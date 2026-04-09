1fa8c95c76c6d0a333344c3a40b7a0195380ecc6
Guilherme Mitsuzumi <mitsuzumiguilhermeryu@gmail.com>
2026-04-08 22:11:20 -0300

    (WIP) feature - Homepage
    Desenvolvimento da Homepage Screen e de widgets reutilizaveis para o funcionamento da página

--- lib/main.dart
+++ lib/main.dart
@@ -18,3 +18,17 @@ class App extends StatelessWidget {
     );
   }
 }
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Home")),
+      body: const Center(
+        child: Text("Bem-vindo!"),
+      ),
+    );
+  }
+}
